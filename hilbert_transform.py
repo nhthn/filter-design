34ef57cdef6fa13d3b3ac004b0af19c5fd5eba4a
Nathan Ho <nathan.waikin.ho@gmail.com>
2020-02-22 21:52:05 -0800

    Add group delay support

--- hilbert_transform.py
+++ hilbert_transform.py
@@ -3,16 +3,19 @@ Weaver, Donald K. 1953. "Design of RC Wide-Band 90-Degree Phase-Difference
 Network."
 Hutchins, Bernie. "The Design of Wideband Analog 90-Degree Phase Differencing
 Networks Without a Large Spread of Capacitor Values."
+Haible, Juergen. "JH FS-1 Frequency Shifter." http://www.jhaible.info/tonline_stuff/hj_fs.html
 """
 import numpy
 import numpy.polynomial.polynomial as poly
 
 
 class PhaseDifferenceNetwork:
-    def __init__(self, poles_a, poles_b, error_in_degrees):
+    def __init__(self, poles_a, poles_b, min_frequency, max_frequency, error_in_degrees):
         self.poles_a = poles_a
         self.poles_b = poles_b
         self.allpass_count = len(self.poles_a) + len(self.poles_b)
+        self.min_frequency = float(min_frequency)
+        self.max_frequency = float(max_frequency)
         self.error_in_degrees = error_in_degrees
 
     def transfer_function(self, frequencies):
@@ -46,21 +49,22 @@ class PhaseDifferenceNetwork:
         we have a sign flip relative to the reference.
         """
         def phase(w, a):
-            return numpy.rad2deg(numpy.pi + 2 * numpy.arctan(w / a))
+            return numpy.pi + 2 * numpy.arctan(w / a)
 
         # Add multiples of 2pi so that the graph is close to 0 at DC
         offset = -numpy.sum(phase(0, self.poles_a))
         offset = 2 * numpy.pi * round(offset / (2 * numpy.pi))
 
+        w = 2 * numpy.pi * frequencies
+
         phase_a = offset
         for pole in self.poles_a:
-            phase_a += phase(frequencies, pole)
+            phase_a += phase(w, pole)
         phase_b = offset
         for pole in self.poles_b:
-            phase_b += phase(frequencies, pole)
-        phase_difference = phase_a - phase_b
+            phase_b += phase(w, pole)
 
-        return phase_a, phase_b, phase_difference
+        return phase_a, phase_b
 
     def plot(self):
         """Plot the phase responses with matplotlib.
@@ -74,11 +78,36 @@ class PhaseDifferenceNetwork:
         plt.ylabel("Phase (deg.)")
         #plt.ylim(-180, 180)
         w = numpy.geomspace(1, 100e3, 2000, endpoint=False)
-        phase_a, phase_b, phase_difference = self.phase(w)
-        plt.plot(w, phase_a, label="Network A phase response")
-        plt.plot(w, phase_b, label="Network B phase response")
-        plt.plot(w, phase_difference, label="Phase difference")
+        phase_a, phase_b = self.phase(w)
+        phase_difference = phase_a - phase_b
+        plt.plot(w, numpy.rad2deg(phase_a), label="Network A phase response")
+        plt.plot(w, numpy.rad2deg(phase_b), label="Network B phase response")
+        plt.plot(w, numpy.rad2deg(phase_difference), label="Phase difference")
         plt.show()
+
+    def get_average_group_delay(self):
+        """Compute the group delay of each network over the specified bandwidth, averaged
+        over the bandwidth and averaged across the two networks.
+
+        This uses a shortcut employing the Fundamental Theorem of Calculus. Since phase
+        response is the antiderivative of group delay (aside from a sign flip), this average
+        can be found by simply computing the phase response at the two endpoints of the
+        interval and dividing by the difference between the endpoints. In formula:
+
+        f1 = min angular frequency
+        f2 = max angular frequency
+        average group delay = -(phase_response(f2) - phase_response(f1)) / (f2 - f1)
+        """
+        endpoints = numpy.array([self.min_frequency, self.max_frequency])
+        # The endpoints are here specified in frequency. When computing this denominator,
+        # the interval size must be converted to angular frequency, hence multiplication by
+        # 2pi.
+        k = 1 / ((endpoints[1] - endpoints[0]) * 2 * numpy.pi)
+        phase_a, phase_b = self.phase(endpoints)
+        group_delay_a = -(phase_a[1] - phase_a[0]) * k
+        group_delay_b = -(phase_b[1] - phase_b[0]) * k
+        average_group_delay = (group_delay_a + group_delay_b) * 0.5
+        return average_group_delay
 
     def print(self):
         """Print all filter coefficients to full precision, as well as some
@@ -88,6 +117,10 @@ class PhaseDifferenceNetwork:
         print("Poles, path A: " + ", ".join([x.astype(str) for x in self.poles_a]))
         print("Poles, path B: " + ", ".join([x.astype(str) for x in self.poles_b]))
         print(f"Error: {self.error_in_degrees:.4}°")
+        print()
+
+        average_group_delay = self.get_average_group_delay()
+        print(f"Average group delay over specified bandwidth: {average_group_delay * 1e3:.4}ms")
 
 def design_weaver_method(min_frequency, max_frequency, n):
     k_prime = min_frequency / max_frequency
@@ -150,11 +183,11 @@ def design_weaver_method_2(
         (q ** 2 - q ** 6) * numpy.sin(4 * phi)
         / (1 + (q ** 2 + q ** 6) * numpy.cos(4 * phi))
     )
-    poles = -min_frequency * numpy.tan(phi - phi_prime) / numpy.sqrt(k_prime)
+    poles = -2 * numpy.pi * min_frequency * numpy.tan(phi - phi_prime) / numpy.sqrt(k_prime)
     poles_a = poles[0::2]
     poles_b = poles[1::2]
 
-    return PhaseDifferenceNetwork(poles_a, poles_b, error_in_degrees)
+    return PhaseDifferenceNetwork(poles_a, poles_b, min_frequency, max_frequency, error_in_degrees)
 
 if __name__ == "__main__":
     filter_ = design_weaver_method_2(20, 20000, n=12)
